3a1e34535409adf49f9a4b3e756a0ea63ddfb588
Reto <reto.scheiwiller5@bluewin.ch>
2020-08-21 11:43:33 +0200

    library cleanup

--- src/main/java/com/xresch/cfw/mail/CFWMail.java
+++ src/main/java/com/xresch/cfw/mail/CFWMail.java
@@ -1,28 +1,7 @@
 package com.xresch.cfw.mail;
 
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.Properties;
 import java.util.logging.Logger;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.Authenticator;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
-import com.xresch.cfw._main.CFW;
 import com.xresch.cfw.logging.CFWLog;
 
 public class CFWMail {
