678f82ef03c71a11176224f2a8d8730f0b6cd05e
Reto <reto.scheiwiller5@bluewin.ch>
2020-09-02 13:13:42 +0200

    re-added mailing again.

--- src/test/java/com/xresch/cfw/tests/mail/MailTest.java
+++ src/test/java/com/xresch/cfw/tests/mail/MailTest.java
@@ -1,19 +1,27 @@
 package com.xresch.cfw.tests.mail;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Tag;
+
+import com.xresch.cfw._main.CFW;
+
+@Tag("development")
 public class MailTest {
 	
-//	//@Test
-//	public void testMail() throws IOException {
-//		CFW.Properties.loadProperties(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
-//	    CFW.Mail.sendFromNoReply("test@pengtoolbox.io", "Testing Subject", "<!DOCTYPE HTML><html>SimpleEmail Testing Body. <strong>STRONG</strong></html>");
-//	}
-//	
-//	//@Test
-//	public void testMailWithAttachment() throws IOException {
-//		CFW.Properties.loadProperties(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
-//		
-//		File attachment = new File(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
-//	    CFW.Mail.sendFromNoReplyWithAttachement("test@pengtoolbox.io", "AttachmentTest", "See <strong>attachment</strong>", attachment);
-//	}
+	//@Test
+	public void testMail() throws IOException {
+		CFW.Properties.loadProperties(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
+	    CFW.Mail.sendFromNoReply("test@pengtoolbox.io", "Testing Subject", "<!DOCTYPE HTML><html>SimpleEmail Testing Body. <strong>STRONG</strong></html>");
+	}
+	
+	//@Test
+	public void testMailWithAttachment() throws IOException {
+		CFW.Properties.loadProperties(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
+		
+		File attachment = new File(CFW.CLI.getValue(CFW.CLI.CONFIG_FILE));
+	    CFW.Mail.sendFromNoReplyWithAttachement("test@pengtoolbox.io", "AttachmentTest", "See <strong>attachment</strong>", attachment);
+	}
 
 }
