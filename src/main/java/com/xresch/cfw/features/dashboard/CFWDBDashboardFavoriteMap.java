590d39fe3ecd3dd44b99dce2ae6aa29c31f2d980
Reto <reto.scheiwiller5@bluewin.ch>
2022-09-21 10:02:35 +0200

    commented not yet used methods

--- src/main/java/com/xresch/cfw/features/dashboard/CFWDBDashboardFavoriteMap.java
+++ src/main/java/com/xresch/cfw/features/dashboard/CFWDBDashboardFavoriteMap.java
@@ -204,26 +204,26 @@ public class CFWDBDashboardFavoriteMap {
 	 * @param user
 	 * @return Hashmap with dashboards(key=user name), or null on exception
 	 ****************************************************************/
-	public static ResultSet selectDashboardsForUserResultSet(User user) {
-		
-		if( user == null) {
-			new CFWLog(logger)
-				.severe("The user cannot be null.");
-			return null;
-		}
-		
-		return new CFWSQL(user)
-				.queryCache(CFWDBDashboardFavoriteMap.class, "selectDashboardsForUserResultSet")
-				.custom(
-					"SELECT P.* "
-					+"FROM CFW_PERMISSION P "
-					+"JOIN CFW_ROLE_PERMISSION_MAP AS GP ON GP.FK_ID_DASHBOARD = P.PK_ID "
-					+"JOIN CFW_USER_ROLE_MAP AS UG ON UG.FK_ID_USER = GP.FK_ID_USER "
-					+"WHERE UG.FK_ID_USER = ?;", 
-					user.id())
-				.getResultSet();
-		
-	}
+//	public static ResultSet selectDashboardsForUserResultSet(User user) {
+//		
+//		if( user == null) {
+//			new CFWLog(logger)
+//				.severe("The user cannot be null.");
+//			return null;
+//		}
+//		
+//		return new CFWSQL(user)
+//				.queryCache(CFWDBDashboardFavoriteMap.class, "selectDashboardsForUserResultSet")
+//				.custom(
+//					"SELECT P.* "
+//					+"FROM CFW_PERMISSION P "
+//					+"JOIN CFW_ROLE_PERMISSION_MAP AS GP ON GP.FK_ID_DASHBOARD = P.PK_ID "
+//					+"JOIN CFW_USER_ROLE_MAP AS UG ON UG.FK_ID_USER = GP.FK_ID_USER "
+//					+"WHERE UG.FK_ID_USER = ?;", 
+//					user.id())
+//				.getResultSet();
+//		
+//	}
 	
 	/***************************************************************
 	 * Retrieve the dashboard overview for all users.
@@ -258,30 +258,31 @@ public class CFWDBDashboardFavoriteMap {
 	 * @param user
 	 * @return Hashmap with users(key=user name, value=user object), or null on exception
 	 ****************************************************************/
-	public static String getDashboardMapForUserAsJSON(String userID) {
-		
-		//----------------------------------
-		// Check input format
-		if(userID == null ^ !userID.matches("\\d+")) {
-			new CFWLog(logger)
-			.severe("The userID '"+userID+"' is not a number.");
-			return "[]";
-		}
-		
-		String sqlString = "SELECT P.PK_ID, P.NAME, P.DESCRIPTION, M.FK_ID_USER AS ITEM_ID, M.IS_DELETABLE FROM "+Dashboard.TABLE_NAME+" P "
-				+ " LEFT JOIN "+CFWDBDashboardFavoriteMap.TABLE_NAME+" M "
-				+ " ON M.FK_ID_DASHBOARD = P.PK_ID"
-				+ " AND M.FK_ID_USER = ?"
-				+ " ORDER BY LOWER(P.NAME)";;
-		
-		ResultSet result = CFWDB.preparedExecuteQuery(sqlString, 
-				userID);
-		
-		String json = ResultSetUtils.toJSON(result);
-		CFWDB.close(result);	
-		return json;
-
-	}
+//	public static String getDashboardMapForUserAsJSON(String userID) {
+//		
+//		//----------------------------------
+//		// Check input format
+//		if(userID == null ^ !userID.matches("\\d+")) {
+//			new CFWLog(logger)
+//			.severe("The userID '"+userID+"' is not a number.");
+//			return "[]";
+//		}
+//		
+//		String sqlString = "SELECT P.PK_ID, P.NAME, P.DESCRIPTION, M.FK_ID_USER AS ITEM_ID, M.IS_DELETABLE FROM "+Dashboard.TABLE_NAME+" P "
+//				+ " LEFT JOIN "+CFWDBDashboardFavoriteMap.TABLE_NAME+" M "
+//				+ " ON M.FK_ID_DASHBOARD = P.PK_ID"
+//				+ " AND M.FK_ID_USER = ?"
+//				+ " ORDER BY LOWER(P.NAME)";;
+//		
+//		ResultSet result = CFWDB.preparedExecuteQuery(sqlString, 
+//				userID);
+//		
+//		String json = ResultSetUtils.toJSON(result);
+//		CFWDB.close(result);	
+//		return json;
+//
+//	}
+	
 	/***************************************************************
 	 * Remove the user from the user if it is a member of the user, 
 	 * add it otherwise.
