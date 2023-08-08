3810c97148ec69c8c5ff461b6e76a36b74a7165b
xresch <reto.scheiwiller5@bluewin.ch>
2023-08-08 15:53:20 +0100

    Query: Tests for command formatrecord

--- src/test/java/com/xresch/cfw/tests/features/query/TestCFWQueryCommands.java
+++ src/test/java/com/xresch/cfw/tests/features/query/TestCFWQueryCommands.java
@@ -70,25 +70,6 @@ public class TestCFWQueryCommands extends DBTestMaster{
 	 * 
 	 ****************************************************************/
 	@Test
-	public void testFilterNullValues() throws IOException {
-		
-		String queryString = CFW.Files.readPackageResource(PACKAGE, "query_testFilterNullValues.txt");
-		
-		CFWQueryResultList resultArray = new CFWQueryExecutor()
-				.parseAndExecuteAll(queryString, earliest, latest, 0);
-		
-		Assertions.assertEquals(1, resultArray.size());
-		
-		// No filtering occurs as all filter conditions are true
-		CFWQueryResult queryResults = resultArray.get(0);
-		Assertions.assertEquals(10000, queryResults.getResultCount());
-
-	}
-	
-	/****************************************************************
-	 * 
-	 ****************************************************************/
-	@Test
 	public void testChart() throws IOException {
 		
 		String queryString = "| source random records=10 type=series\r\n" + 
@@ -369,67 +350,21 @@ public class TestCFWQueryCommands extends DBTestMaster{
 	 * 
 	 ****************************************************************/
 	@Test
-	public void test_Top() throws IOException {
+	public void testFilterNullValues() throws IOException {
 		
-		//---------------------------------
-		String queryString = "| source random records=1000 | top 123"
-				+ ";| source random records=1000 | top 321"
-				;
+		String queryString = CFW.Files.readPackageResource(PACKAGE, "query_testFilterNullValues.txt");
 		
 		CFWQueryResultList resultArray = new CFWQueryExecutor()
 				.parseAndExecuteAll(queryString, earliest, latest, 0);
 		
-		// 2 results for first and top
-		Assertions.assertEquals(2, resultArray.size());
+		Assertions.assertEquals(1, resultArray.size());
 		
-		// First returns 123 results
+		// No filtering occurs as all filter conditions are true
 		CFWQueryResult queryResults = resultArray.get(0);
-		JsonArray queryResultsArray = queryResults.getResults();
-		Assertions.assertEquals(123, queryResultsArray.size());
-		Assertions.assertEquals(0, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
-		Assertions.assertEquals(122, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
-		
-		// top returns 321 results
-		queryResults = resultArray.get(1);
-		queryResultsArray = queryResults.getResults();
-		Assertions.assertEquals(321, queryResultsArray.size());
-		Assertions.assertEquals(0, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
-		Assertions.assertEquals(320, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
+		Assertions.assertEquals(10000, queryResults.getResultCount());
+	
 	}
-	
-	/****************************************************************
-	 * 
-	 ****************************************************************/
-	@Test
-	public void test_Tail() throws IOException {
-		
-		//---------------------------------
-		String queryString = "| source random records=1000 | tail 123"
-				+ ";| source random records=1000 | tail 321"
-				;
-		
-		CFWQueryResultList resultArray = new CFWQueryExecutor()
-				.parseAndExecuteAll(queryString, earliest, latest, 0);
-		
-		// 2 results for last and tail
-		Assertions.assertEquals(2, resultArray.size());
-		
-		// last returns 123 results
-		CFWQueryResult queryResults = resultArray.get(0);
-		JsonArray queryResultsArray = queryResults.getResults();
-		Assertions.assertEquals(123, queryResultsArray.size());
-		Assertions.assertEquals(999-122, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
-		Assertions.assertEquals(999, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
-		
-		// tail returns 321 results 
-		queryResults = resultArray.get(1);
-		queryResultsArray = queryResults.getResults();
-		Assertions.assertEquals(321, queryResultsArray.size());
-		Assertions.assertEquals(999-320, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
-		Assertions.assertEquals(999, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
-	}
-	
-	
+
 	/****************************************************************
 	 * 
 	 ****************************************************************/
@@ -552,10 +487,6 @@ public class TestCFWQueryCommands extends DBTestMaster{
 //			]
 //		}
 
-
-
-		
-		// 
 		JsonObject displaySettings = resultArray.get(0).getDisplaySettings();
 		JsonObject fieldFormats = displaySettings.get("fieldFormats").getAsJsonObject();
 		
@@ -588,13 +519,65 @@ public class TestCFWQueryCommands extends DBTestMaster{
 			);
 	}
 	
+	/****************************************************************
+	 * 
+	 ****************************************************************/
+	@Test
+	public void testFormatrecord() throws IOException {
+		
+		//---------------------------------
+		String queryString = "| source random records=100\r\n" + 
+				"| formatrecord \r\n" + 
+				"	[(FIRSTNAME ~='^He'), \"#332288\"] \r\n" + 
+				"	[(VALUE >= 60), \"red\", \"yellow\"] \r\n" + 
+				"	[(LIKES_TIRAMISU==true), \"green\"] \r\n" + 
+				"	[true, \"cfw-gray\"]\r\n" + 
+				"| formatfield LIKES_TIRAMISU=none"
+				;
+		
+		CFWQueryResultList resultArray = new CFWQueryExecutor()
+				.parseAndExecuteAll(queryString, earliest, latest, 0);
+		
+		// 3 results for distinct, dedup and uniq
+		Assertions.assertEquals(1, resultArray.size());
+		
+//		ALL RECORDS SHOULD CONTAIN FOLLOWING FIELDS
+//		{
+//		     ...
+//			 "_bgcolor": "red",
+//			 "_textcolor": "yellow"
+//		},
+		
+		//-----------------------------------
+		// Iterate Results Check has correct format
+		CFWQueryResult queryResults = resultArray.get(0);
+		Assertions.assertEquals(100, queryResults.getResultCount());
+		for(int i = 0; i < 100 && i < queryResults.getResultCount(); i++) {
+			JsonObject current = queryResults.getResult(i);
+			
+			if(!current.get("FIRSTNAME").isJsonNull()
+			&& current.get("FIRSTNAME").getAsString().startsWith("He")) {
+				Assertions.assertEquals("#332288", current.get("_bgcolor").getAsString(), "Record has field _bgcolor=#332288");
+			}else if(current.get("VALUE").getAsInt() >= 60) {
+				Assertions.assertEquals("red", current.get("_bgcolor").getAsString(), "Record has field _bgcolor=#332288");
+				Assertions.assertEquals("yellow", current.get("_textcolor").getAsString(), "Record has field _bgcolor=red and _textcolor=yellow");
+			}else if(!current.get("LIKES_TIRAMISU").isJsonNull() 
+					&& current.get("LIKES_TIRAMISU").getAsBoolean()) {
+				Assertions.assertEquals("green", current.get("_bgcolor").getAsString(), "Record has field _bgcolor=green");
+			}else {
+				Assertions.assertEquals("cfw-gray", current.get("_bgcolor").getAsString(), "Record has field _bgcolor=cfw-gray");
+			}
+			
+		}
+	}
+	
 	
 	/****************************************************************
 	 * 
 	 ****************************************************************/
 	@Test
-	public void testGlobals_Metadata() throws IOException {
-		
+	public void testGlobals() throws IOException {
+			
 		//---------------------------------
 		String queryString = 
 				  "| globals multidisplay=4 myCustomProperty=MyCustomValue\r\n"
@@ -622,25 +605,128 @@ public class TestCFWQueryCommands extends DBTestMaster{
 		//------------------------------
 		// Check First Query Result
 		JsonObject globals  = resultArray.get(0).getGlobals();
-		JsonObject metadata = resultArray.get(0).getMetadata();
-		
+
 		//Assertions.assertTrue(globals.get("earliest").isJsonPrimitive());
 		//Assertions.assertTrue(globals.get("latest").isJsonPrimitive());
 		Assertions.assertEquals("MyCustomValue", globals.get("myCustomProperty").getAsString());
+		
+		//------------------------------
+		// Check Second Query Result
+		globals  =  resultArray.get(1).getGlobals();
+
+		//Assertions.assertTrue(globals.get("earliest").isJsonPrimitive());
+		//Assertions.assertTrue(globals.get("latest").isJsonPrimitive());
+		Assertions.assertEquals("MyCustomValue", globals.get("myCustomProperty").getAsString());
+
+	}
+
+	/****************************************************************
+	 * 
+	 ****************************************************************/
+	@Test
+	public void testMetadata() throws IOException {
+		
+		//---------------------------------
+		String queryString = 
+				  "| globals multidisplay=4 myCustomProperty=MyCustomValue\r\n"
+				+ "| metadata name='Default Table' firstQueryProp='hello' | source random type=default records=5 | display as=table \r\n"
+				+ ";\r\n"
+				+ "| metadata name='Bigger Number Table' secondQueryProp='world' | source random type=numbers records=5 | display as=biggertable "
+				;
+		
+		CFWQueryResultList resultArray = new CFWQueryExecutor()
+				.parseAndExecuteAll(queryString, earliest, latest, 0);
+		
+		// 2 query results
+		Assertions.assertEquals(2, resultArray.size());
+
+//		===== EXPECTED IN BOTH RESULTS ====		
+//		globals: {
+//		  "earliest": 1640365794703,
+//		  "latest": 1645722594704,
+//		  "myCustomProperty": "MyCustomValue"
+//		}
+//		metadata: {
+//		    "name": "Default Table"
+//		}
+
+		//------------------------------
+		// Check First Query Result
+		JsonObject metadata = resultArray.get(0).getMetadata();
+		
 		Assertions.assertEquals("Default Table", metadata.get("name").getAsString());
 		Assertions.assertEquals("hello", metadata.get("firstQueryProp").getAsString());
 		
 		//------------------------------
 		// Check Second Query Result
-		globals  =  resultArray.get(1).getGlobals();
 		metadata = resultArray.get(1).getMetadata();
 		
-		//Assertions.assertTrue(globals.get("earliest").isJsonPrimitive());
-		//Assertions.assertTrue(globals.get("latest").isJsonPrimitive());
-		Assertions.assertEquals("MyCustomValue", globals.get("myCustomProperty").getAsString());
 		Assertions.assertEquals("Bigger Number Table", metadata.get("name").getAsString());
 		Assertions.assertEquals("world", metadata.get("secondQueryProp").getAsString());
 		
+	}
+	/****************************************************************
+	 * 
+	 ****************************************************************/
+	@Test
+	public void test_Tail() throws IOException {
+		
+		//---------------------------------
+		String queryString = "| source random records=1000 | tail 123"
+				+ ";| source random records=1000 | tail 321"
+				;
+		
+		CFWQueryResultList resultArray = new CFWQueryExecutor()
+				.parseAndExecuteAll(queryString, earliest, latest, 0);
+		
+		// 2 results for last and tail
+		Assertions.assertEquals(2, resultArray.size());
+		
+		// last returns 123 results
+		CFWQueryResult queryResults = resultArray.get(0);
+		JsonArray queryResultsArray = queryResults.getResults();
+		Assertions.assertEquals(123, queryResultsArray.size());
+		Assertions.assertEquals(999-122, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
+		Assertions.assertEquals(999, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
+		
+		// tail returns 321 results 
+		queryResults = resultArray.get(1);
+		queryResultsArray = queryResults.getResults();
+		Assertions.assertEquals(321, queryResultsArray.size());
+		Assertions.assertEquals(999-320, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
+		Assertions.assertEquals(999, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
+	}
+
+	/****************************************************************
+	 * 
+	 ****************************************************************/
+	@Test
+	public void test_Top() throws IOException {
+		
+		//---------------------------------
+		String queryString = "| source random records=1000 | top 123"
+				+ ";| source random records=1000 | top 321"
+				;
+		
+		CFWQueryResultList resultArray = new CFWQueryExecutor()
+				.parseAndExecuteAll(queryString, earliest, latest, 0);
+		
+		// 2 results for first and top
+		Assertions.assertEquals(2, resultArray.size());
+		
+		// First returns 123 results
+		CFWQueryResult queryResults = resultArray.get(0);
+		JsonArray queryResultsArray = queryResults.getResults();
+		Assertions.assertEquals(123, queryResultsArray.size());
+		Assertions.assertEquals(0, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
+		Assertions.assertEquals(122, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
+		
+		// top returns 321 results
+		queryResults = resultArray.get(1);
+		queryResultsArray = queryResults.getResults();
+		Assertions.assertEquals(321, queryResultsArray.size());
+		Assertions.assertEquals(0, queryResultsArray.get(0).getAsJsonObject().get("INDEX").getAsInt());
+		Assertions.assertEquals(320, queryResultsArray.get(queryResultsArray.size()-1).getAsJsonObject().get("INDEX").getAsInt());
 	}
 		
 	
