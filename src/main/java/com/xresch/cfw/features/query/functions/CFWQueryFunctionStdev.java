012f24ae0f14dcafa2af1106d5bd38dacec1c99c
xresch <reto.scheiwiller5@bluewin.ch>
2024-02-06 14:36:53 +0100

    Query: fixed and improved stdev function

--- src/main/java/com/xresch/cfw/features/query/functions/CFWQueryFunctionStdev.java
+++ src/main/java/com/xresch/cfw/features/query/functions/CFWQueryFunctionStdev.java
@@ -1,6 +1,7 @@
 package com.xresch.cfw.features.query.functions;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Map.Entry;
@@ -14,6 +15,7 @@ import com.xresch.cfw.features.query.CFWQueryFunction;
 import com.xresch.cfw.features.query.EnhancedJsonObject;
 import com.xresch.cfw.features.query.FeatureQuery;
 import com.xresch.cfw.features.query.parse.QueryPartValue;
+import com.xresch.cfw.utils.CFWMath;
 
 /************************************************************************************************************
  * 
@@ -24,7 +26,7 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 
 	public static final String FUNCTION_NAME = "stdev";
 	private ArrayList<BigDecimal> values = new ArrayList<BigDecimal>(); 
-	private BigDecimal sum = new BigDecimal(0); 
+	private BigDecimal sum = new BigDecimal(0).setScale(6); 
 	
 	private boolean isAggregated = false;
 	private int precision = 3;
@@ -74,8 +76,9 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 	@Override
 	public String descriptionSyntaxDetailsHTML() {
 		return "<p><b>valueOrFieldname:&nbsp;</b>The value or fieldname used for the average.</p>"
-			 + "<p><b>includeNulls:&nbsp;</b>(Optional)Toggle if null values should be included in the average(Default:false).</p>"
+			 + "<p><b>includeNulls:&nbsp;</b>(Optional)Toggle if null values should be included as zero in the average(Default:false).</p>"
 			 + "<p><b>precision:&nbsp;</b>(Optional)Decimal precision of the result.(Default:3)</p>"
+			 + "<p><b>usePopulation:&nbsp;</b>(Optional)If true use population formula, if false use sample formula.(Default: true)</p>"
 			;
 	}
 
@@ -113,10 +116,11 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 	/***********************************************************************************************
 	 * 
 	 ***********************************************************************************************/
-	private QueryPartValue calculateStandardDeviation() {
+	private BigDecimal calculateStandardDeviation(boolean usePopulation) {
 		
-		if(values.size() == 0) {
-			return QueryPartValue.newNumber(0);
+		// zero or one number will have standard deviation 0
+		if(values.size() <= 1) {
+			return BigDecimal.ZERO;
 		}
 	
 //		How to calculate standard deviation:
@@ -129,6 +133,8 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 		//-----------------------------------------
 		// STEP 1: Find Average
 		BigDecimal count = new BigDecimal(values.size());
+		count.setScale(6);
+		
 		BigDecimal average = sum.divide(count, RoundingMode.HALF_UP);
 		
 		BigDecimal sumDistanceSquared = BigDecimal.ZERO;
@@ -136,7 +142,7 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 			//-----------------------------------------
 			// STEP 2: For each data point, find the 
 			// square of its distance to the mean.
-			BigDecimal distance = value.subtract(average).abs();
+			BigDecimal distance = value.subtract(average);
 			//-----------------------------------------
 			// STEP 3: Sum the values from Step 2.
 			sumDistanceSquared = sumDistanceSquared.add(distance.pow(2));
@@ -144,19 +150,19 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 		
 		//-----------------------------------------
 		// STEP 4 & 5: Divide and take square root
-		BigDecimal divided = sumDistanceSquared.divide(count, RoundingMode.HALF_UP);
+		
+		BigDecimal divisor = (usePopulation) ? count : count.subtract(BigDecimal.ONE);
+		
+		BigDecimal divided = sumDistanceSquared.divide(divisor, RoundingMode.HALF_UP);
 		
 		// TODO JDK8 Migration: should work with JDK 9
-		// MathContext mc = new MathContext(6, RoundingMode.HALF_UP);
-		// BigDecimal standardDeviation = divided.sqrt(mc);
-		BigDecimal standardDeviation = 
-				new BigDecimal(Math.sqrt(divided.doubleValue()))
-				.setScale(precision, RoundingMode.HALF_UP);
+		MathContext mc = new MathContext(6, RoundingMode.HALF_UP);
+		BigDecimal standardDeviation = divided.sqrt(mc);
 		
 		//reset values when calculation is done
 		values.clear();
-		sum = new BigDecimal(0);
-		return QueryPartValue.newNumber(standardDeviation);
+		sum = BigDecimal.ZERO.setScale(6);
+		return standardDeviation;
 	}
 	
 	/***********************************************************************************************
@@ -172,12 +178,7 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 		if(parameters.size() > 1) {
 			countNulls = parameters.get(1).getAsBoolean();
 		}
-		
-		if(parameters.size() > 2
-		&& parameters.get(2).isNumberOrNumberString()) {
-			precision = parameters.get(2).getAsInteger();
-		}
-		
+				
 		addValueToAggregation(value, countNulls);
 	
 	}
@@ -189,10 +190,20 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 	@Override
 	public QueryPartValue execute(EnhancedJsonObject object, ArrayList<QueryPartValue> parameters) {
 		
+		//----------------------------
+		// Handle usePopulation
+		boolean usePopulation = true;
+		if(parameters.size() > 3
+		&& parameters.get(3).isBoolOrBoolString()) {
+			usePopulation = parameters.get(3).getAsBoolean();
+		}
+		
+		
+		BigDecimal result = BigDecimal.ZERO;
 		if(isAggregated) {			
-			return calculateStandardDeviation();
+			result = calculateStandardDeviation(usePopulation);
 		}else if(parameters.size() == 0) {
-			return QueryPartValue.newNull();
+			result = null;
 		}else {
 			
 			QueryPartValue param = parameters.get(0);
@@ -211,7 +222,7 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 					QueryPartValue value = QueryPartValue.newFromJsonElement(array.get(i));
 					addValueToAggregation(value, countNulls);
 				}
-				return calculateStandardDeviation();
+				result = calculateStandardDeviation(usePopulation);
 				
 			}else if(param.isJsonObject()) {
 				
@@ -219,16 +230,28 @@ public class CFWQueryFunctionStdev extends CFWQueryFunction {
 					QueryPartValue value = QueryPartValue.newFromJsonElement(entry.getValue());
 					addValueToAggregation(value, countNulls);
 				}
-				return calculateStandardDeviation();
+				result = calculateStandardDeviation(usePopulation);
 			}else if(param.isNumberOrNumberString()) {
 				
 				addValueToAggregation(param, countNulls);
-				return calculateStandardDeviation();
+				result = calculateStandardDeviation(usePopulation);
 			}
 			
 		}
 		
-		//return null in all other cases
-		return QueryPartValue.newNull();
+		//----------------------------
+		// Handle Precision
+		if(parameters.size() > 2
+		&& parameters.get(2).isNumberOrNumberString()) {
+			precision = parameters.get(2).getAsInteger();
+		}
+		
+		//----------------------------
+		// Return Value
+		if(result == null) {
+			return QueryPartValue.newNull();
+		}else {
+			return QueryPartValue.newNumber(result.setScale(precision, RoundingMode.HALF_UP));
+		}
 	}
 }
